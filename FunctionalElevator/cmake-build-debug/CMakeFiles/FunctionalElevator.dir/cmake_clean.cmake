f56c7993267024c6ce3f4f874c782ec37d3c8d20
Divstar07 <123ndukachijindu@gmail.com>
2024-03-22 00:17:07 -0400

    hmm

---
+++ FunctionalElevator/cmake-build-debug/CMakeFiles/FunctionalElevator.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FunctionalElevator.dir/main.c.obj"
+  "CMakeFiles/FunctionalElevator.dir/main.c.obj.d"
+  "FunctionalElevator.exe"
+  "FunctionalElevator.exe.manifest"
+  "FunctionalElevator.pdb"
+  "libFunctionalElevator.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FunctionalElevator.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ FunctionalElevator/cmake-build-debug/CMakeFiles/FunctionalElevator.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for FunctionalElevator.
+# This may be replaced when dependencies are built.
